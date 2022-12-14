b4039e472900803c1cefc7f7c50f7b17c516ee4d
Sai Kumar <prasanna.kandregula.lpk@gmail.com>
2022-12-14 17:08:30 +0530

    Refractor: Moved the rendering of component logic to a common place in the test file.

--- src/App.test.js
+++ src/App.test.js
@@ -6,15 +6,17 @@ import {
 } from "./constants/testConstants";
 import App from "./App";
 
-test("Board should be available with 9 sqaures when the Game starts.", () => {
+beforeEach(() => {
   render(<App />);
+});
+
+test("Board should be available with 9 sqaures when the Game starts.", () => {
   const squares = screen.queryAllByTestId(SQUARE);
 
   expect(squares).toHaveLength(9);
 });
 
 test("Player X Should start the game", () => {
-  render(<App />);
   expect(screen.getByTestId(CURRENT_TURN_ACCESSOR).textContent).toEqual(
     CURRENT_TURN_TEXT
   );
